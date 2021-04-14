1e70a37dcffe67772a4b8c7773f1409a110ffa03
PavelRoR <pavelvp2182@yandex.ru>
2021-04-14 11:04:27 +0500

    Course page is stylezed

--- _app/_js/course.js
+++ _app/_js/course.js
@@ -1,13 +1,26 @@
-$(document).ready(function() {
-        /* Якорь */
-        $("a[href='#plan']").click(function (h) {
-            h.preventDefault();
-            var f = $(this).attr("href"),
-                g = $(f).offset().top;
-            $("body,html").animate({
-                scrollTop: g
-            }, 1500)
+$(document).ready(function () {
+    /* Якорь */
+    $("a[href='#plan']").click(function (h) {
+        h.preventDefault();
+        var f = $(this).attr("href"),
+            g = $(f).offset().top;
+        $("body,html").animate({
+            scrollTop: g
+        }, 1500)
+    });
+    $(function (){
+        var mT = $('.module-title');
+        mT.on('click', function () {
+            // mT.removeClass('active').next().slideUp(500);
+            if (!$(this).hasClass('active')) {
+                mT.removeClass('active').next().slideUp(500);
+                $(this).addClass('active').next().slideDown(500);
+            } else {
+                mT.removeClass('active').next().slideUp(500);
+                // $(this).removeClass('active').next().slideUp(500);
+            }
         });
+    });
     $('.text-rev-link').magnificPopup({
         type: 'image',
         mainClass: 'mfp-fade',
@@ -28,14 +41,12 @@ $(document).ready(function() {
         // adaptiveHeight: true,
         centerMode: true,
         appendArrows: '.video-revs-arrows-1',
-        responsive: [
-        {
+        responsive: [{
             breakpoint: 768,
             settings: {
                 slidesToShow: 1
             }
-        }
-        ]
+        }]
     });
     $('.revs-slider-text').slick({
         autoplay: false,
@@ -50,14 +61,12 @@ $(document).ready(function() {
         // adaptiveHeight: true,
         centerMode: true,
         appendArrows: '.video-revs-arrows-2',
-        responsive: [
-        {
+        responsive: [{
             breakpoint: 561,
             settings: {
                 slidesToShow: 1
             }
-        }
-        ]
+        }]
     });
     $('.slick-dots').wrap('<div class="container container-revs">')
     /* Видео */
@@ -65,7 +74,8 @@ $(document).ready(function() {
         var a = $(this).parent().attr("data-youtube");
         $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&modestbranding=1&mute=1&showinfo=0&rel=0" allowfullscreen></iframe>')
     });
-    function imToVideo(){
+
+    function imToVideo() {
         $('.video-wrapper-revs iframe').each(function () {
             var l = $(this).parent().attr('data-img');
             $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
@@ -75,14 +85,14 @@ $(document).ready(function() {
             $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?modestbrandig=1&mute=1&showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>');
         });
     }
-$('.revs-slider').on('swipe', function (event, slick, direction) {
-    imToVideo();
-});
-$('.revs-slider').on('afterChange', function (event, slick, direction) {
-    imToVideo();
-});
-$('.revs-slider').on('beforeChange', function (event, slick, direction) {
-    imToVideo();
-});
-/*Конец документа*/
+    $('.revs-slider').on('swipe', function (event, slick, direction) {
+        imToVideo();
+    });
+    $('.revs-slider').on('afterChange', function (event, slick, direction) {
+        imToVideo();
+    });
+    $('.revs-slider').on('beforeChange', function (event, slick, direction) {
+        imToVideo();
+    });
+    /*Конец документа*/
 });

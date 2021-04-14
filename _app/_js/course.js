47344eb588bfdc415dbbe226663214d1afce85a8
PavelRoR <pavelvp2182@yandex.ru>
2021-04-14 10:00:09 +0500

    Course page is typeseted

---
+++ _app/_js/course.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+        /* Якорь */
+        $("a[href='#plan']").click(function (h) {
+            h.preventDefault();
+            var f = $(this).attr("href"),
+                g = $(f).offset().top;
+            $("body,html").animate({
+                scrollTop: g
+            }, 1500)
+        });
+    $('.text-rev-link').magnificPopup({
+        type: 'image',
+        mainClass: 'mfp-fade',
+        gallery: {
+            enabled: true
+        }
+    });
+    $('.revs-slider').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '40px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        appendArrows: '.video-revs-arrows-1',
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+        ]
+    });
+    $('.revs-slider-text').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '40px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        appendArrows: '.video-revs-arrows-2',
+        responsive: [
+        {
+            breakpoint: 561,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+        ]
+    });
+    $('.slick-dots').wrap('<div class="container container-revs">')
+    /* Видео */
+    $(".video-wrapper-revs img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&modestbranding=1&mute=1&showinfo=0&rel=0" allowfullscreen></iframe>')
+    });
+    function imToVideo(){
+        $('.video-wrapper-revs iframe').each(function () {
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+        });
+        $(".video-wrapper-revs img").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?modestbrandig=1&mute=1&showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>');
+        });
+    }
+$('.revs-slider').on('swipe', function (event, slick, direction) {
+    imToVideo();
+});
+$('.revs-slider').on('afterChange', function (event, slick, direction) {
+    imToVideo();
+});
+$('.revs-slider').on('beforeChange', function (event, slick, direction) {
+    imToVideo();
+});
+/*Конец документа*/
+});
